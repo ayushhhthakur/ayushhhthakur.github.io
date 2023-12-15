ebb53618b3816cb7e0bdf1da9c66b4482bfaddf6
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-12-15 20:21:37 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 let isMenuOpen = false;
 
+
 const toggleMenu = () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
